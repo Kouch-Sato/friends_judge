2e0e56294353e82bcdf91d79d1302effe992dee5
Kouch-Sato <redkouchred@gmail.com>
2019-06-29 20:28:13 +0900

    annotate

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,11 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id      :bigint           not null, primary key
+#  user_id :integer
+#
+
 class Book < ApplicationRecord
   belongs_to :user
   has_many :reviews

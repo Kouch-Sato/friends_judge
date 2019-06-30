53eab17b4aa5350b4feccc39c4ae0ccfb861295c
Kouch-Sato <redkouchred@gmail.com>
2019-06-30 10:54:13 +0900

    annotate

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,14 +2,14 @@
 #
 # Table name: reviews
 #
-#  id         :bigint           not null, primary key
-#  book_id    :integer
-#  type       :integer
-#  content    :string(255)
-#  status     :integer
-#  user_id    :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :bigint           not null, primary key
+#  book_id     :integer
+#  review_type :integer
+#  content     :string(255)
+#  status      :integer
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
 #
 
 class Review < ApplicationRecord
